b674de69ae3581209931deff0374d2efcbd3314f
Adam J. Stewart <ajstewart426@gmail.com>
2020-04-28 13:18:38 -0500

    Add initial training script

---
+++ train.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""machine learning model for predicting ocean bathymetry"""
+
+import argparse
+
+
+def set_up_parser():
+    """Set up the argument parser.
+
+    Returns:
+        argparse.ArgumentParser: the argument parser
+    """
+    # Initialize new parser
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # Generic arguments
+    parser.add_argument(
+        '-d', '--data-dir', default='data/CRUST1.0',
+        help='directory containing CRUST1.0 dataset', metavar='DIR')
+
+    # Model subparser
+    subparsers = parser.add_subparsers(
+        required=True, help='machine learning model')
+
+    lstsq_parser = subparsers.add_parser('lstsq', help='least squares')
+
+    return parser
+
+
+def main(args):
+    """High-level pipeline.
+
+    Trains the model and evaluates performance.
+
+    Parameters:
+        args (argparse.Namespace): command-line arguments
+    """
+    pass
+
+
+if __name__ == '__main__':
+    # Parse supplied arguments
+    parser = set_up_parser()
+    args = parser.parse_args()
+
+    main(args)
