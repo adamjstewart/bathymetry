745ecbe8d968a70b0d599c1b49bf91deb3703551
Adam J. Stewart <ajstewart426@gmail.com>
2020-04-29 12:36:14 -0500

    Don't need xyz-bd3 data

--- datasets/crust.py
+++ datasets/crust.py
@@ -9,14 +9,14 @@ import numpy as np
 import pandas as pd
 
 
-def read_data(data_dir):
+def read_data(data_dir: str) -> pd.DataFrame:
     """Read CRUST 1.0 dataset.
 
     Parameters:
-        data_dir (str): directory containing dataset
+        data_dir: directory containing dataset
 
     Returns:
-        pandas.DataFrame: the data
+        the data
     """
     assert os.path.isdir(data_dir)
 
@@ -31,6 +31,7 @@ def read_data(data_dir):
     }
 
     # Boundary topography
+    # These files come with the CRUST 1.0 base model
     bnds = pd.read_fwf(os.path.join(data_dir, 'crust1.bnds'), **kwargs)
 
     # P/S-wave velocity
@@ -41,28 +42,23 @@ def read_data(data_dir):
     rho = pd.read_fwf(os.path.join(data_dir, 'crust1.rho'), **kwargs)
 
     # Crust type
+    # This file comes with the CRUST 1.0 add-on
     ctype = np.loadtxt(
         os.path.join(data_dir, 'CNtype1-1.txt'), dtype=np.object)
     ctype = ctype.flatten()
     ctype = pd.get_dummies(ctype, dtype=np.float32)  # one-hot encoding
 
     # Age
+    # This file is downsampled from EarthByte
+    # TODO: directly use EarthByte dataset with a different data loader
     kwargs['names'] = ['longitude', 'latitude', 'age']
     age = pd.read_table(os.path.join(data_dir, 'age1.txt'), **kwargs)
 
-    # Bathymetry
-    kwargs['names'] = ['longitude', 'latitude', 'bathymetry']
-    bath = pd.read_table(os.path.join(data_dir, 'xyz-bd3_oprm'), **kwargs)
-
     # Combine data
-    data = pd.concat([bnds, vp, vs, rho, ctype, age, bath], axis=1, keys=[
+    data = pd.concat([bnds, vp, vs, rho, ctype, age], axis=1, keys=[
         'boundary topograpy', 'p-wave velocity', 's-wave velocity',
-        'density', 'crust type', 'age', 'bathymetry'], sort=False)
+        'density', 'crust type', 'age'], sort=False)
     data.set_index([('age', 'latitude'), ('age', 'longitude')], inplace=True)
-    data.drop([('bathymetry', 'longitude'), ('bathymetry', 'latitude')],
-              axis=1, inplace=True)
     data.index.rename(['latitude', 'longitude'], inplace=True)
-
-    assert isinstance(data, pd.DataFrame)
 
     return data
