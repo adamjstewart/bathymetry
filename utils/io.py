b78d50556b59002c9eb0e5cd0c891ca2febec303
Adam J. Stewart <ajstewart426@gmail.com>
2020-09-16 15:36:40 -0500

    Add more IO methods

--- utils/io.py
+++ utils/io.py
@@ -30,3 +30,30 @@ def load_pickle(directory: str, model: str) -> pd.Series:
     """
     path = os.path.join(directory, model + '.pkl')
     return pd.read_pickle(path)
+
+
+def save_csv(data: pd.Series, directory: str, model: str):
+    """Save a pandas Series as a CSV file.
+
+    Parameters:
+        data: the pandas Series to save
+        directory: the checkpoint directory
+        model: the name of the model
+    """
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, model + '.csv')
+    data.to_csv(path)
+
+
+def load_csv(directory: str, model: str) -> pd.Series:
+    """Load a CSV file as a pandas Series.
+
+    Parameters:
+        directory: the checkpoint directory
+        model: the name of the model
+
+    Returns:
+        the pandas Series
+    """
+    path = os.path.join(directory, model + '.csv')
+    return pd.read_csv(path)
