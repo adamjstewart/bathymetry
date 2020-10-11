6d2bb854a74cc3f7862fa0a6fe3c5e5e02257c6e
Adam J. Stewart <ajstewart426@gmail.com>
2020-10-11 14:57:40 -0500

    Return dict of performance

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,17 +1,21 @@
 """Regression metrics for model evaluation."""
 
 import math
+from typing import Dict
 
 import pandas as pd
 from sklearn.metrics import mean_squared_error, r2_score
 
 
-def evaluate(labels: pd.Series, predictions: pd.Series):
+def evaluate(labels: pd.Series, predictions: pd.Series) -> Dict[str, float]:
     """Prints the model performance evaluation.
 
     Parameters:
         labels: the ground truth labels
         predictions: the model predictions
+
+    Returns:
+        dictionary containing RMSE and R^2
     """
     assert isinstance(labels, pd.Series)
     assert isinstance(predictions, pd.Series)
@@ -22,3 +26,8 @@ def evaluate(labels: pd.Series, predictions: pd.Series):
 
     print('RMSE:', rmse)
     print('R^2: ', r2)
+
+    return {
+        'RMSE': rmse,
+        'R^2': r2,
+    }
