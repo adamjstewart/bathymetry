94a6aeb0f22c466380f1e01b7ac287e37145b509
Adam J. Stewart <ajstewart426@gmail.com>
2020-10-11 11:33:04 -0500

    Fix bug in ablation study when nothing to drop

--- preprocessing/reduce.py
+++ preprocessing/reduce.py
@@ -43,14 +43,16 @@ def ablation_study(data: pd.DataFrame, labels: Sequence) -> pd.DataFrame:
     """
     assert isinstance(data, pd.DataFrame)
 
-    for label in labels.split(','):
-        if label in [
-            'thickness', 'p-wave velocity', 's-wave velocity', 'density', 'age'
-        ]:
-            level = 0
-        else:
-            level = 1
-        data = data.drop(columns=label, level=level)
+    if labels is not None:
+        for label in labels.split(','):
+            if label in [
+                'thickness', 'p-wave velocity', 's-wave velocity',
+                'density', 'age'
+            ]:
+                level = 0
+            else:
+                level = 1
+            data = data.drop(columns=label, level=level)
 
     assert isinstance(data, pd.DataFrame)
 
