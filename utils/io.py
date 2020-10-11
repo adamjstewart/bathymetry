b438e783bee26d8ddb820712105bde2a5d69d0a8
Adam J. Stewart <ajstewart426@gmail.com>
2020-10-11 14:43:44 -0500

    More generic pickling

--- utils/io.py
+++ utils/io.py
@@ -1,59 +1,67 @@
 """Collection of input/output utilities."""
 
 import os
+import pickle
+from typing import Any
 
 import pandas as pd
 
 
-def save_pickle(data: pd.Series, directory: str, model: str):
-    """Save a pandas Series as a pickle.
+def save_pickle(data: Any, directory: str, filename: str):
+    """Write the pickled representation of data to filename.
 
     Parameters:
-        data: the pandas Series to save
-        directory: the checkpoint directory
-        model: the name of the model
+        data: the data to save
+        directory: the directory to save to
+        filename: the filename to save to
     """
+    path = os.path.join(directory, filename + '.pickle')
+    print(f'Writing {path}...')
     os.makedirs(directory, exist_ok=True)
-    path = os.path.join(directory, model + '.pkl')
-    data.to_pickle(path)
+    with open(path, 'wb') as f:
+        pickle.dump(data, f)
 
 
-def load_pickle(directory: str, model: str) -> pd.Series:
-    """Load a pickled object as a pandas Series.
+def load_pickle(directory: str, filename: str) -> Any:
+    """Read the pickled representation of data from filename.
 
     Parameters:
-        directory: the checkpoint directory
-        model: the name of the model
+        directory: the directory to load from
+        filename: the filename to load from
 
     Returns:
-        the pandas Series
+        the original data
     """
-    path = os.path.join(directory, model + '.pkl')
-    return pd.read_pickle(path)
+    path = os.path.join(directory, filename + '.pickle')
+    print(f'Reading {path}...')
+    with open(path, 'rb') as f:
+        return pickle.load(f)
 
 
-def save_csv(data: pd.Series, directory: str, model: str):
+def save_csv(data: pd.Series, directory: str, filename: str):
     """Save a pandas Series as a CSV file.
 
     Parameters:
         data: the pandas Series to save
-        directory: the checkpoint directory
-        model: the name of the model
+        directory: the directory to save to
+        model: the filename to save to
     """
+    path = os.path.join(directory, filename + '.csv')
+    print(f'Writing {path}...')
     os.makedirs(directory, exist_ok=True)
-    path = os.path.join(directory, model + '.csv')
     data.to_csv(path)
 
 
-def load_csv(directory: str, model: str) -> pd.Series:
+def load_csv(directory: str, filename: str) -> pd.Series:
     """Load a CSV file as a pandas Series.
 
     Parameters:
-        directory: the checkpoint directory
-        model: the name of the model
+        directory: the directory to load from
+        filename: the filename to load from
 
     Returns:
         the pandas Series
     """
-    path = os.path.join(directory, model + '.csv')
+    path = os.path.join(directory, filename + '.csv')
+    print(f'Reading {path}...')
     return pd.read_csv(path)
