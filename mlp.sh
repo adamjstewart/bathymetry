1b7645b3b7f8c243f6fc019bc9654ac4cf1bae11
Adam J. Stewart <ajstewart426@gmail.com>
2021-05-29 12:13:08 -0500

    Slurm updates

--- mlp.sh
+++ mlp.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=ljliu
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 
 spack --color=never env activate ~/bathymetry
 
@@ -15,5 +15,5 @@ spack --color=never env activate ~/bathymetry
 ulimit -u 10000
 
 srun="srun --exclusive -N1 -n1"
-parallel="parallel --delay 0.2 -j $SLURM_NTASKS --joblog runtask.log --resume-failed --verbose"
+parallel="parallel --delay 0.2 -j $SLURM_NTASKS --joblog runtask-mlp.log --resume-failed --verbose"
 $parallel "$srun python $HOME/bathymetry/train.py mlp --activation {1} --solver {2} --alpha {3} --hidden-layers {4} --hidden-size {5} --learning-rate {6}" ::: logistic tanh relu ::: lbfgs sgd adam ::: 0.0001 0.001 0.01 0.1 ::: 3 4 5 6 7 ::: 8 16 32 64 128 256 512 ::: 0.01 0.001 0.0001
--- svr.sh
+++ svr.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=ljliu
-#SBATCH --time=2:00:00
+#SBATCH --time=24:00:00
 
 spack --color=never env activate ~/bathymetry
 
@@ -15,5 +15,5 @@ spack --color=never env activate ~/bathymetry
 ulimit -u 10000
 
 srun="srun --exclusive -N1 -n1"
-parallel="parallel --delay 0.2 -j $SLURM_NTASKS --joblog runtask.log --resume-failed --verbose"
+parallel="parallel --delay 0.2 -j $SLURM_NTASKS --joblog runtask-svr.log --resume-failed --verbose"
 $parallel "$srun python $HOME/bathymetry/train.py svr --kernel {1} --gamma {2} --coef0 {3} --c {4} --epsilon {5}" ::: linear poly rbf sigmoid ::: scale auto ::: 0 0.01 0.1 1 ::: 0.5 1 2 4 ::: 0 0.01 0.1 1
