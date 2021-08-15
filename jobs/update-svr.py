945bad12c98e8d52d21fb78c255716432a06f3af
Adam J. Stewart <ajstewart426@gmail.com>
2021-08-15 10:36:50 -0500

    flake8 + black

--- jobs/update-svr.py
+++ jobs/update-svr.py
@@ -4,8 +4,8 @@ import glob
 
 
 matches = []
-for f in glob.iglob('checkpoints/checkpoint-svr-*'):
-    parts = f.split('-')
+for f in glob.iglob("checkpoints/checkpoint-svr-*"):
+    parts = f.split("-")
     c = float(parts[2])
     coef0 = float(parts[4])
     epsilon = float(parts[6])
@@ -14,8 +14,8 @@ for f in glob.iglob('checkpoints/checkpoint-svr-*'):
     match = (kernel, gamma, coef0, c, epsilon)
     matches.append(match)
 
-with open('svr-job-file.txt') as fo:
-    with open('svr-job-file.txt-new', 'w') as fn:
+with open("svr-job-file.txt") as fo:
+    with open("svr-job-file.txt-new", "w") as fn:
         for line in fo:
             parts = line.split()
             kernel = parts[4]
